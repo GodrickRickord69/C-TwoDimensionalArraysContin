3a732c5bb29bc3c3345157acbef2d65c548ec66e
Nikita <varashilov125@gmail.com>
2023-07-08 19:00:13 +0300

    Task3 произведение матриц

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,38 +6,29 @@
 18 20
 15 18*/
 
-Console.Write("Введите количество сторок 1-й матрицы: ");
-int rows1 = int.Parse(Console.ReadLine()!);
+int[,] Matr1 = GetRandomMatr(4, 4, 0, 10);
+PrintMatr(Matr1);
 
-Console.Write("Введите количество столбцов 1-й матрицы: ");
-int columns1 = int.Parse(Console.ReadLine()!);
+int[,] Matr2 = GetRandomMatr(4, 4, 0, 10);
+PrintMatr(Matr2);
 
-Console.Write("Введите количество сторок 2-й матрицы: ");
-int rows2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Произведение первой и второй матриц:");
+MultiplyMatrix(Matr1, Matr2);
 
-Console.Write("Введите количество столбцов 2-й матрицы: ");
-int columns2 = int.Parse(Console.ReadLine()!);
-
-int[,] Matr1 = GetMatr1(4, 4, 0, 10);
-PrintMatr1(Matr1);
-
-int[,] Matr2 = GetMatr2(4, 4, 0, 10);
-PrintMatr2(Matr2);
-
-
-int[,] GetMatr1(int m, int n, int MinValue, int MaxValue)
+int[,] GetRandomMatr(int m, int n, int MinValue, int MaxValue)
 {
-    int[,] Matr1 = new int[m, n];
+    int[,] Matr = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            Matr1[i, j] = new Random().Next(MinValue, MaxValue);
+            Matr[i, j] = new Random().Next(MinValue, MaxValue);
         }
     }
-    return Matr1;
+    return Matr;
 }
-void PrintMatr1(int[,] array)
+
+void PrintMatr(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -46,81 +37,25 @@ void PrintMatr1(int[,] array)
             Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
-    }
-}
-
-int[,] GetMatr2(int m, int n, int MinValue, int MaxValue)
-{
-    int[,] Matr2 = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            Matr2[i, j] = new Random().Next(MinValue, MaxValue);
-        }
-    }
-    return Matr2;
-}
-void PrintMatr2(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
-    }
-}
-
-int[,] resultMatr = new int[m, n];
-
-MultiplyMatrix(Matr1, Matr2, resultMatr);
-Console.WriteLine($"Произведение первой и второй матриц:");
-WriteArray(resultMatr);
-
-void MultiplyMatrix(int[,] Matr1, int[,] Matr2, int[,] resultMatr)
-{
-  for (int i = 0; i < resultMatr.GetLength(0); i++)
-  {
-    for (int j = 0; j < resultMatr.GetLength(1); j++)
-    {
-      int sum = 0;
-      for (int k = 0; k < Matr1.GetLength(1); k++)
-      {
-        sum += Matr1[i,k] * Matr2[k,j];
-      }
-      resultMatr[i,j] = sum;
-    }
-  }
-}
-
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      array[i, j] = new Random().Next();
-    }
-  }
-}
-
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      Console.Write(array[i,j] + " ");
     }
     Console.WriteLine();
-  }
+}
+
+void MultiplyMatrix(int[,] Matr1, int[,] Matr2)
+{
+    int[,] resultMatr = new int[Matr1.GetLength(0), Matr1.GetLength(1)];
+
+    for (int i = 0; i < resultMatr.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatr.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < resultMatr.GetLength(1); k++)
+            {
+                sum += Matr1[i, k] * Matr2[k, j];
+            }
+            resultMatr[i, j] = sum;
+        }
+    }
+    PrintMatr(resultMatr);
 }
